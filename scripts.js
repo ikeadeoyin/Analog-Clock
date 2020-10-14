576a7555479d0cfef10d134eae75f9a173bd14fd
ikeadeoyin <ikeadeoyin@gmail.com>
2020-10-14 14:47:37 +0100

    analog clock project

---
+++ scripts.js
@@ -0,0 +1,27 @@
+let hourHand = document.querySelector("#hour");
+let minuteHand = document.querySelector("#minute");
+let secondHand = document.querySelector("#second");
+
+let date = new Date();
+console.log(date);
+let hr = date.getHours();
+let min = date.getMinutes();
+let sec = date.getMinutes();
+console.log(`hours:${hr}, minutes:${min}`);
+
+let hrPosition = (hr*360/12) + (min*(360/60)/12);
+let minPosition = (min*360/60) + (sec*(360/60)/60);
+let secPosition = (sec * 360) /60;
+
+
+function runTheClock() {
+   hrPosition = hrPosition + (3/360);
+   minPosition = minPosition + (6/60);
+   secPosition = secPosition + 6;
+
+    hourHand.style.transform = "rotate(" + hrPosition + "deg)";
+    minuteHand.style.transform = "rotate(" + minPosition + "deg)";//`rotate(${minPostion}deg)`;
+    secondHand.style.transform = "rotate(" + secPosition + "deg)"; //`rotate(${secPostion}deg)`;
+}
+
+let interval = setInterval(runTheClock, 1000);
